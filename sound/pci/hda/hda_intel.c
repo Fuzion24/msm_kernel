8f191bbc0d30a50827a092426c42186c4c65e5b8
Harsha Priya <harshapriya.n@intel.com>
2014-10-29 12:13:13 +0000

    CHROMIUM: ALSA: Revert "BACKPORT: ALSA: hda - Fix runtime PM check"
    This reverts commit 90e15c821b1b3204703bec488850672ae3775f4e.
    The runtime PM check fix caused HDMI audio not working after hotplug
    when the device is on DC power.  It was meant to fix a bug that won't
    happen in a production system.
    BUG=chrome-os-partner:33350
    TEST=Verified on Auron with and without DC power
    
    Signed-off-by: Harsha Priya <harshapriya.n@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/217447
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    (cherry picked from commit 4947d403342f6c9910702df09d6852e60ab9ed13)
    Reviewed-on:: https://chromium-review.googlesource.com/217814
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    
    Change-Id: I10e8b15396823b78da50fe3a157a9eb9d4a6cbca
    Reviewed-on: https://chromium-review.googlesource.com/226332
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-by: HARSHAPRIYA N <harshapriya.n@intel.com>
    Tested-by: HARSHAPRIYA N <harshapriya.n@intel.com>
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    
    Conflicts:
    	sound/pci/hda/hda_intel.c
    [filbranden: 3.18 rebase. Solved trivial conflicts.]
    [TODO(filbranden): Ask dgreid@ whether this needs to be kept in our tree.]
    Signed-off-by: Filipe Brandenburger <filbranden@chromium.org>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1090,13 +1090,8 @@ static int register_vga_switcheroo(struct azx *chip)
  */
 static int azx_free(struct azx *chip)
 {
-	struct pci_dev *pci = chip->pci;
 	struct hda_intel *hda = container_of(chip, struct hda_intel, chip);
 	int i;
-
-	if ((chip->driver_caps & AZX_DCAPS_PM_RUNTIME)
-			&& chip->running)
-		pm_runtime_get_noresume(&pci->dev);
 
 	azx_del_card_list(chip);
 
@@ -1875,6 +1870,8 @@ static int azx_probe(struct pci_dev *pci,
 	if (schedule_probe)
 		schedule_work(&hda->probe_work);
 
+	if (pci_dev_run_wake(pci))
+		pm_runtime_put_noidle(&pci->dev);
 	dev++;
 	if (chip->disabled)
 		complete_all(&hda->probe_wait);
@@ -1894,7 +1891,6 @@ static unsigned int azx_max_codecs[AZX_NUM_DRIVERS] = {
 static int azx_probe_continue(struct azx *chip)
 {
 	struct hda_intel *hda = container_of(chip, struct hda_intel, chip);
-	struct pci_dev *pci = chip->pci;
 	int dev = chip->dev_index;
 	int err;
 
@@ -1967,8 +1963,6 @@ static int azx_probe_continue(struct azx *chip)
 	power_down_all_codecs(chip);
 	azx_notifier_register(chip);
 	azx_add_card_list(chip);
-	if ((chip->driver_caps & AZX_DCAPS_PM_RUNTIME) || hda->use_vga_switcheroo)
-		pm_runtime_put_noidle(&pci->dev);
 
 out_free:
 	if (err < 0)
@@ -1980,6 +1974,8 @@ out_free:
 static void azx_remove(struct pci_dev *pci)
 {
 	struct snd_card *card = pci_get_drvdata(pci);
+	if (pci_dev_run_wake(pci))
+		pm_runtime_get_noresume(&pci->dev);
 
 	if (card)
 		snd_card_free(card);
